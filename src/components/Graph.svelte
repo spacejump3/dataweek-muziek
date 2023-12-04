3a2e198458502c7a294fd204b2372bc2f7dd1439
Stephan <112871518+spacejump3@users.noreply.github.com>
2023-12-04 15:41:13 +0100

    file structure change

---
+++ src/components/Graph.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { onMount } from 'svelte';
+    import * as d3 from 'd3';
+
+    onMount(async () => {
+        const svg = d3.select('svg');
+        const data = ['1', '2', '3', '4', '5', '6'];
+
+        svg.selectAll('rect').data(data).join('rect')
+            .attr('width', 100)
+            .attr('height', 100)
+            .attr('x', (d, i) => i * 160);
+    });
+</script>
+
+<p>Hello lmao</p>
+<p>brooooooooooooo</p>
+
+<svg width='1000'></svg>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<h1>Welcome to SvelteKit</h1>
+<script>
+    import Graph from '../components/Graph.svelte'
+</script>
+
+<body>
+    <Graph />
+</body>
