5dc11f3477b12ecd8fd2869c236e71fcb680e973
Stephan <112871518+spacejump3@users.noreply.github.com>
2023-12-08 03:04:13 +0100

    posters added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,20 +3,31 @@
 </script>
 
 <body>
-    <div>
+    <div class="introText">
         <h1>Hoe vaak weten artiesten niet wat ze moeten zeggen?</h1>
         <p>
             Hoor je ook niet veelste vaak al die <em>oh's</em>, <em>ahh's</em>
             of misschien zelfs <em>grrahs</em> in je favoriete pop nummers? Het
-            voelt altijd een beetje als filler tekst. Dit wordt ook wel een vocalise genoemd, waarbij er wordt gezongen zonder echte woorden te gebruiken. Wij hebben een bubble
-            chart gemaakt van hoe vaak een vocalise wordt gebruikt van
-            zes verschillende artiesten. Van elke artiest hebben we een album uit 2023 gekozen. Sleep je cursor
-            over de cirkels om exact te zien hoe vaak ze een specifiek woord
-            zeggen en je zult zien <em>hoe vaak</em> artiesten niet weten wat ze
-            moeten zeggen. Geniet van de resultaten!
+            voelt altijd een beetje als filler tekst. Dit wordt ook wel een
+            vocalise genoemd, waarbij er wordt gezongen zonder echte woorden te
+            gebruiken. Wij hebben een bubble chart gemaakt van hoe vaak een
+            vocalise wordt gebruikt van zes verschillende artiesten. Van elke
+            artiest hebben we een album uit 2023 gekozen. Sleep je cursor over
+            de cirkels om exact te zien hoe vaak ze een specifiek woord zeggen
+            en je zult zien <em>hoe vaak</em> artiesten niet weten wat ze moeten
+            zeggen. Geniet van de resultaten!
         </p>
     </div>
     <BubbleChart />
+    <h2>Posters</h2>
+    <div class="images">
+        <img src="../img/doja_cat_graph.png" alt="" />
+        <img src="../img/drake_graph.png" alt="" />
+        <img src="../img/janelle_graph.png" alt="" />
+        <img src="../img/olivia_rodrigo_graph.png" alt="" />
+        <img src="../img/post_malone_graph.png" alt="" />
+        <img src="../img/troye_sivan_graph.png" alt="" />
+    </div>
 </body>
 
 <footer>Gemaakt door Stephan en Michelle &copy; 2023</footer>
@@ -31,9 +42,20 @@
             rgba(28, 27, 31, 1) 0%,
             rgb(0, 0, 0) 100%
         );
+        display: flex;
+        flex-direction: column;
     }
 
-    div {
+    img {
+        width: 30em;
+    }
+
+    .images {
+        max-width: 100em;
+        margin: auto;
+    }
+
+    .introText {
         margin: auto;
         width: 50em;
     }
